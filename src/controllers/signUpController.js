259e2ff82227ae7b7b2aa6484548eb66b32a451a
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2022-03-02 18:04:40 -0300

    FEAT: Add funcionalities in post of sign up route

---
+++ src/controllers/signUpController.js
@@ -0,0 +1,43 @@
+import connection from "../db";
+import bcrypt from "bcrypt"
+
+export async function postSignUp (request, response){
+
+    try {
+
+        const {nome, email, senha, senhaConfirmada} = request.body;
+
+        if(!nome){
+
+            return response.sendStatus(400)
+        }
+
+        const findEmailQuery = connection.query(`
+            SELECT * FROM usuarios
+            WHERE email = $1`, [email]);
+        
+        if(findEmailQuery.rows > 0){
+            return response.sendStatus(422);
+        }
+
+        if(senha !== senhaConfirmada){
+            return response.sendStatus(422);
+        }
+
+        const passwordHashed = bcrypt.hashSync(senha, 10);
+        const insertUserQuery = await connection.query(`
+            INSERT INTO usuarios (nome, email, senha)
+            VALUES ($1, $2, $3)`, [nome, email.toLowerCase(), passwordHashed]);
+
+        if(!insertUserQuery){
+            return response.sendStatus(401);
+        }
+
+        response.sendStatus(200);
+        
+    } catch (error) {
+
+        response.status(500).send(error);
+    }
+    
+}
--- src/schemas/userSignInSchema.js
+++ src/schemas/userSignInSchema.js
@@ -4,7 +4,7 @@ const userSignInSchema = joi.object({
     nome: joi.string().pattern(/^[a-záàâãéèêíïóôõöúçñ ,.'-]+$/i).required(),
     email: joi.string().email().required(),
     senha: joi.string().required(),
-    confirmPassword: joi.string().required()
+    senhaConfirmada: joi.string().required()
 });
 
 export default userSignInSchema;
